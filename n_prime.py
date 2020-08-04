85995a1b640b5a7f04752131399c9f247778ef1d
tejamarneni <63079069+tejamarneni@users.noreply.github.com>
2020-08-04 15:41:38 -0400

    Add files via upload

---
+++ n_prime.py
@@ -0,0 +1,19 @@
+prime_list = []
+
+def primelist(num):
+    i = 0
+    j = 3
+    while i <= num:
+        d = 0
+        for k in range(3,int(j**0.5)+1,2):
+            if j % k == 0:
+                d += 1
+        if d == 0:
+            prime_list.append(j)
+            i += 1
+        j += 2
+    return prime_list     
+
+# prints n prime numbers
+print(primelist(10000))
+         
