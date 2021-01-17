5de8f3c927530849b1e3a47a37013aee5117f8db
Teja Marneni <63079069+tejamarneni@users.noreply.github.com>
2021-01-17 08:40:08 -0500

    Add files via upload

---
+++ random_game.py
@@ -0,0 +1,28 @@
+import random
+
+def sec_num():
+    s_num = random.randint(0,101)
+    try:
+        c = 0
+        while True:
+            u_num = int(input("Enter a number:\n"))
+            if u_num > 0 and u_num > s_num:
+                print("To Big. Try again")
+                c += 1
+                continue
+            elif u_num > 0 and u_num < s_num:
+                print("Too small. Try again")
+                c += 1
+                continue
+            elif u_num < 0:
+                print("Please Enter a Positive Number.") 
+                c += 1
+                continue
+            else:
+                c += 1
+                print(f"Congratulations.You got in {c} trials")
+                break 
+    except ValueError:
+        print("Your Entry is invalid. Please enter integer numbers only.")        
+
+sec_num()
