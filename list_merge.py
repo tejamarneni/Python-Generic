934f7452059437f703bb9c70c1b7cf922c09ea29
Teja Marneni <63079069+tejamarneni@users.noreply.github.com>
2024-09-19 17:43:35 -0400

    Add files via upload

---
+++ list_merge.py
@@ -0,0 +1,15 @@
+def merge_sorted_lists(list1, list2):
+    merged = []
+    i, j = 0, 0
+    while i < len(list1) and j < len(list2):
+        if list1[i] <= list2[j]:
+            merged.append(list1[i])
+            i += 1
+        else:
+            merged.append(list2[j])
+            j += 1
+    merged.extend(list1[i:])
+    merged.extend(list2[j:])
+    return merged
+
+print(merge_sorted_lists([1,2,4,5],[3,6,9]))
