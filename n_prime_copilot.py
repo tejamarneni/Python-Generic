44d28934362872ea257e9d017d0fb781963ab143
Teja Marneni <63079069+tejamarneni@users.noreply.github.com>
2024-09-23 10:48:37 -0400

    Update n_prime_copilot.py

--- n_prime_copilot.py
+++ n_prime_copilot.py
@@ -1,1 +1,18 @@
+def primelist(num):
+  prime_list = [2]
 
+  j = 3
+  while len(prime_list) < num:
+    is_prime = True
+    for k in range(3, int(j**0.5) + 1, 2):
+      if j % k == 0:
+        is_prime = False
+
+    if is_prime:
+      prime_list.append(j)
+    J += 2
+
+  return prime_list
+
+print(primelist(15))
+                    
